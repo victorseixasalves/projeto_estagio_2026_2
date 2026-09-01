bcb98e4ed0bb24d3151352be98133d12868a8205
victorseixasalves <vseixasalves@gmail.com>
2026-08-31 21:24:18 -0300

    Configuração fillable e casts do model "Socio"

--- app/Models/Socio.php
+++ app/Models/Socio.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Socio extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'tipo',
+        'data',
+        'setor',
+        'status',
+    ];
+
+    protected $casts = [
+        'data' => 'date',
+    ];
 }
