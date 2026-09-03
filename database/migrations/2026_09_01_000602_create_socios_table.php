61f978e04abee92bfec848b91247c6c73d85a5c1
victorseixasalves <vseixasalves@gmail.com>
2026-09-03 16:03:22 -0300

    Alteração de estrutura para "planos" e "setores" serem editáveis pelo admin

--- database/migrations/2026_09_01_000602_create_socios_table.php
+++ database/migrations/2026_09_01_000602_create_socios_table.php
@@ -6,37 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('socios', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->string('email');
-            $table->enum('tipo', [
-                'Tradição Tricolor',
-                'Guerreiro do Laranjeiras',
-                'Eterno Campeão',
-                'FluKids',
-            ]);
+            $table->string('tipo');
             $table->date('data');
-            $table->enum('setor', [
-                'Norte',
-                'Sul',
-                'Maracanã Mais',
-                'Visitante',
-            ]);
+            $table->string('setor');
             $table->enum('status', ['pendente', 'confirmado', 'cancelado'])
                 ->default('pendente');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('socios');
